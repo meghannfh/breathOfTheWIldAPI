068d53432bc810d39e7632c6039756813e9828e8
meghann <meghannfh@gmail.com>
2022-05-17 15:42:21 +0900

    May 17 edited CSS info container + js fetch description and name

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 //Example fetch
 document.querySelector('button').addEventListener('click', getFetch)
+const itemCreatureName = document.getElementById('itemCreatureName')
+const itemCreatureDescription = document.getElementById('itemCreatureDescription')
+const infoContainer = document.getElementById('infoContainer')
+const img = document.getElementById('image')
 
 function getFetch(){
 let entry = document.querySelector('input').value
@@ -8,8 +12,14 @@ console.log(entry)
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        console.log(data);
         console.log(data.data.image)
-        document.getElementById('image').src = data.data.image
+
+        img.src = data.data.image
+        itemCreatureName.innerText = data.data.name
+        itemCreatureDescription.innerText = data.data.description
+
+        infoContainer.classList.add('show')
       })
       .catch(err => {
           console.log(`error ${err}`)
