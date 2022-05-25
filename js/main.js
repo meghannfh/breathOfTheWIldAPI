ef367b3708d3784fad2262245069c52a8d062a96
meghann <meghannfh@gmail.com>
2022-05-26 00:19:53 +0900

    added button categories

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const imgContainer = document.getElementById('image')
 const rightArrow = document.getElementById('rightArrow')
 const leftArrow = document.getElementById('leftArrow')
 const arrows = document.querySelectorAll('.arrow')
+const btnContainer = document.getElementById('btnContainer')
 let categoryType;
 let backOrForward;
 let count = 0;
@@ -27,12 +28,72 @@ arrows.forEach((arrow)=>{
 const CREATURES_URL = 'https://botw-compendium.herokuapp.com/api/v2'
   // const url = `https://botw-compendium.herokuapp.com/api/v2/entry/${entry}`;
 
+fetch(CREATURES_URL)
+.then(res => res.json())
+.then(data => {
+  const categories = Object.keys(data.data)
+      console.log(categories)
+
+      for(let i = 0; i < categories.length; i++){
+        const newBtn = document.createElement('button');
+
+        newBtn.innerText = categories[i]
+        btnContainer.appendChild(newBtn)
+      }
+      
+    })
+.catch(err => {
+  console.log(`error ${err}`)
+});
+
+// newBtn.forEach((btn) => {
+//   btn.addEventListener('click', event =>{
+//     let CATEGORY_URL = `https://botw-compendium.herokuapp.com/api/v2/category/${event.target.value}`
+
+//     getFetch(CATEGORY_URL)
+//   })
+// })
+
+// const changeCategory = CATEGORY_URL => {
+//   fetch(CATEGORY_URL)
+//   .then(res => {
+//     res.json()
+//   })
+//   .then(data =>{
+
+//     console.log(data.data)
+    
+//   })
+// }
+
+// newBtn.addEventListener('click', event => {
+//   let CATEGORY_URL = `https://botw-compendium.herokuapp.com/api/v2/category/${event.target.value}`
+
+//   pullDataFromCategory(CATEGORY_URL)
+// })
+
+// const pullDataFromCategory = url =>{
+//   fetch(url)
+//   .then(res => res.json())
+//   .then(data => {
+//     let creaturesArrayNonFood = data.data.creatures.non_food
+//       console.log(creaturesArrayNonFood[count].name)
+//       imgContainer.src = creaturesArrayNonFood[count].image
+//       itemCreatureName.innerText = creaturesArrayNonFood[count].name  
+//   })
+//   .catch(err => {
+//     console.log(`error ${err}`)
+//   });
+//   count++;
+//   console.log(count)
+// }
 
 function getFetch(){
   fetch(CREATURES_URL)
     .then(res =>res.json()) // parse response as JSON
     .then(data => {
       console.log(data.data)
+
       let creaturesArrayNonFood = data.data.creatures.non_food
       console.log(creaturesArrayNonFood[count].name)
       imgContainer.src = creaturesArrayNonFood[count].image
